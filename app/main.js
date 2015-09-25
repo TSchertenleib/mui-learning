1976b21edf1d26133b4522c46841a407207b383d
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-09-25 16:20:53 +0200

    - header & footer fixed
    - funktion, die wartet, bis animation beendet ist

--- app/main.js
+++ app/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function ()   {
 	$(".list li").click(function(){
-	    $(this).toggleClass('haken');
-	    $(this).toggleClass('animate');
+		$(this).toggleClass('haken');
+		$(this).toggleClass('groesse');
+		$(this).toggleClass('aktiv');
 	});
 
+
+	$(".button").click(function(e){
+		$(this).addClass("animation-scale");
+		$(this).one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+			function(event) {
+
+				location.href = $(this).data('href');
+			});
+	});
 });
+
+
+
