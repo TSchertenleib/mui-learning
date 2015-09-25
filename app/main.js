4ecac1d8e0d63337de6d1d41e807a4f5435d07b5
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2015-09-25 12:01:56 +0200

    init jquery

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function ()   {
-$(".list li").click(function(){
-    $(this).toggleClass('haken');
-});
+	$(".list li").click(function(){
+	    $(this).toggleClass('haken');
+	    $(this).toggleClass('animate');
+	});
+
 });
